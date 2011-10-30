c491ae1d9e28e198c372a9db6333a78d8997755e
Ari Epstein <aepstein607@gmail.com>
2011-10-30 09:12:19 -0400

    Fix last commit.

--- lib/cornell-assemblies-rails/rails/searchable.rb
+++ lib/cornell-assemblies-rails/rails/searchable.rb
@@ -5,7 +5,7 @@ module CornellAssembliesRails
         def search_methods( *methods )
           unless method_defined? :_search_methods
             instance_eval do
-              def ransack_with_searchable( params={}, options={} )
+              def search( params={}, options={} )
                 s = _search_methods.inject( scoped ) do |scope, method|
                   param = params.delete( method ) || params.delete( method.to_s )
                   if param
@@ -14,11 +14,10 @@ module CornellAssembliesRails
                     scope
                   end
                 end
-                s.ransack_without_searchable( params, options )
+                s.ransack( params, options )
               end
             end
             cattr_accessor :_search_methods
-            alias_method_chain :ransack, :searchable
             self._search_methods = []
           end
           self._search_methods += methods.flatten
