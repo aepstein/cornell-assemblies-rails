0e2b3fadf586f428c7a9d176aeae03a3e8d84b5c
Ari Epstein <aepstein607@gmail.com>
2011-10-30 08:45:39 -0400

    Fix last commit.

--- lib/cornell-assemblies-rails/rails/searchable.rb
+++ lib/cornell-assemblies-rails/rails/searchable.rb
@@ -3,29 +3,23 @@ module CornellAssembliesRails
     module Searchable
       module ClassMethods
         def search_methods( *methods )
-          instance_eval do
-            def ransack_with_searchable( params={}, options={} )
-              search_methods.each do |method|
-                param = params.delete( method ) || params.delete( method.to_s )
-                if param
-                  send method, param
-                end
-              end
-              ransack_without_searchable( params, options )
-            end
-          end
           unless defined? _search_methods
-            send :include, InstanceMethods
+            instance_eval do
+              def ransack_with_searchable( params={}, options={} )
+                search_methods.each do |method|
+                  param = params.delete( method ) || params.delete( method.to_s )
+                  if param
+                    send method, param
+                  end
+                end
+                ransack_without_searchable( params, options )
+              end
+            end
+            cattr_accessor :_search_methods
+            alias_method_chain :ransack, :searchable
             self._search_methods = []
           end
           self._search_methods += methods.flatten
-        end
-      end
-
-      module InstanceMethods
-        class << self
-          cattr_accessor :_search_methods
-          alias_method_chain :ransack, :searchable
         end
       end
 
