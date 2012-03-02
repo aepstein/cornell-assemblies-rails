db1e2fc74ee614e4a14c8f05eda59f6e3f145c4b
Ari Epstein <ate2@cornell.edu>
2012-03-01 19:46:26 -0500

    Try adding autoload for custom formtastic inputs.

--- lib/cornell-assemblies-rails/rails.rb
+++ lib/cornell-assemblies-rails/rails.rb
@@ -5,6 +5,8 @@ module CornellAssembliesRails
     autoload :Notifiable, 'cornell-assemblies-rails/rails/notifiable'
     autoload :Authenticable, 'cornell-assemblies-rails/rails/authenticable'
     autoload :Searchable, 'cornell-assemblies-rails/rails/searchable'
+    autoload :DatePickerInput, 'app/inputs/date_picker_input'
+    autoload :DateTimePickerInput, 'app/inputs/date_time_picker_input'
     require 'cornell-assemblies-rails/rails/engine'
     require 'cornell-assemblies-rails/version'
   end
