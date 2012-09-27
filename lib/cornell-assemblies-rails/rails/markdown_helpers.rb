f009304e77d824798e1b487b42dfd987d634e8f2
Ari Epstein <ate2@cornell.edu>
2012-09-27 15:01:40 -0400

    Allow tables to come through.

--- lib/cornell-assemblies-rails/rails/markdown_helpers.rb
+++ lib/cornell-assemblies-rails/rails/markdown_helpers.rb
@@ -3,7 +3,8 @@ module CornellAssembliesRails
     module MarkdownHelpers
 
       def markdown( content )
-        sanitize Markdown.new(content, tables: true).to_html
+        sanitize Markdown.new(content, tables: true),
+          tags: %w( table thead tbody th tr td )
       end
 
     end
