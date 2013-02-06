8db7a55cfc48c25b890989b278fbe0437a042416
Ari Epstein <aepstein607@gmail.com>
2013-02-06 08:07:43 -0500

    Fix last commit.

--- lib/app/inputs/date_time_picker_input.rb
+++ lib/app/inputs/date_time_picker_input.rb
@@ -7,7 +7,7 @@ class DateTimePickerInput < SimpleForm::Inputs::StringInput
 
   def input_html_options
     value = object.send(attribute_name)
-    super.merge { value: object.send(attribute_name).try( :to_formatted_s, :us_short ) }
+    super.merge( { value: object.send(attribute_name).try( :to_formatted_s, :us_short ) } )
   end
 
   def input_html_classes
