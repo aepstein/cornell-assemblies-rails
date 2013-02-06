abd1cff287d4c005bbdb0a952fd48ba96bb90454
Ari Epstein <aepstein607@gmail.com>
2013-02-06 08:13:32 -0500

    Date picker formatting done properly.

--- lib/app/inputs/date_picker_input.rb
+++ lib/app/inputs/date_picker_input.rb
@@ -1,7 +1,13 @@
 class DatePickerInput < SimpleForm::Inputs::StringInput
   def input
     super +
-    @builder.hidden_field(attribute_name, { class: attribute_name.to_s + "-alt"})
+    @builder.hidden_field(attribute_name, {  class: ( attribute_name.to_s + "-alt" ),
+      value: object.send(attribute_name).try( :to_formatted_s, :rfc822 ) } )
+  end
+
+  def input_html_options
+    value = object.send(attribute_name)
+    super.merge( { value: object.send(attribute_name).try( :to_formatted_s, :us_short ) } )
   end
 
   def input_html_classes
