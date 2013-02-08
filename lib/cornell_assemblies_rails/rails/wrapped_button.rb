96b6b8dc6c48d524fbe557da84be887b2d15d06f
Ari Epstein <aepstein607@gmail.com>
2013-02-08 13:46:07 -0500

    Handle label argument for wrapped button.

--- lib/cornell_assemblies_rails/rails/wrapped_button.rb
+++ lib/cornell_assemblies_rails/rails/wrapped_button.rb
@@ -7,18 +7,25 @@ module CornellAssembliesRails
           loading = self.object.new_record? ? I18n.t('simple_form.creating') : I18n.t('simple_form.updating')
           options[:"data-loading-text"] = [loading, options[:"data-loading-text"]].compact
           options[:class] = ['btn-primary', options[:class]].compact
+          label = options.delete :label
           args << options
+          primary = if label
+            args.unshift label
+            button(*args, &block)
+          else
+            submit(*args, &block)
+          end
           if cancel = options.delete(:cancel)
             if cancel == "close-modal"
-              submit(*args, &block) + ' ' + I18n.t('simple_form.buttons.or') + ' ' +
+              primary + ' ' + I18n.t('simple_form.buttons.or') + ' ' +
                 template.link_to(I18n.t('simple_form.buttons.cancel'), "#",
                                  { "data-dismiss" => "modal", "aria-hidden" => "true" })
             else
-              submit(*args, &block) + ' ' + I18n.t('simple_form.buttons.or') + ' ' +
+              primary + ' ' + I18n.t('simple_form.buttons.or') + ' ' +
                 template.link_to(I18n.t('simple_form.buttons.cancel'), cancel)
             end
           else
-            submit(*args, &block)
+            primary
           end
         end
       end
