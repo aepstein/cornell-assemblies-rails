f14b7479c7df36b11cc588d7254d2928e31f3c52
Ari Epstein <aepstein607@gmail.com>
2013-02-12 14:59:43 -0500

    Refine last commit.

--- lib/app/inputs/autocomplete_input.rb
+++ lib/app/inputs/autocomplete_input.rb
@@ -1,8 +1,10 @@
 class AutocompleteInput < SimpleForm::Inputs::StringInput
   def input_html_options
     data_options = { url: options[:url] }
-    data_options[:dynamic_term] = options[:dynamic_term]
-    data_options[:dynamic_id] = options[:dynamic_id]
+    if options[:dynamic_term] && options[:dynamic_id]
+      data_options[:dynamic_term] = options[:dynamic_term]
+      data_options[:dynamic_id] = options[:dynamic_id]
+    end
     super.merge( data: data_options )
   end
 
