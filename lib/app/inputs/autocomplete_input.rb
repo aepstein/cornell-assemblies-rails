7391eb59820240a1ab807c37f8c74cd54dd4577f
Ari Epstein <aepstein607@gmail.com>
2013-02-12 14:58:37 -0500

    Try not deleting options in autocomplete.

--- lib/app/inputs/autocomplete_input.rb
+++ lib/app/inputs/autocomplete_input.rb
@@ -1,9 +1,9 @@
 class AutocompleteInput < SimpleForm::Inputs::StringInput
   def input_html_options
-    data_options = { url: options.delete(:url) }
-    data_options[:dynamic_term] = options.delete(:dynamic_term)
-    data_options[:dynamic_id] = options.delete(:dynamic_id)
-    super.merge( { data: data_options } )
+    data_options = { url: options[:url] }
+    data_options[:dynamic_term] = options[:dynamic_term]
+    data_options[:dynamic_id] = options[:dynamic_id]
+    super.merge( data: data_options )
   end
 
   def input_html_classes
