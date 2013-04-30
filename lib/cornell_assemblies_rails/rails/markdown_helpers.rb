5af1cd664c376524741a6d394291afcb2d265d3b
Ari Epstein <aepstein607@gmail.com>
2013-04-30 09:32:42 -0400

    Fix markdown helpers.

--- lib/cornell_assemblies_rails/rails/markdown_helpers.rb
+++ lib/cornell_assemblies_rails/rails/markdown_helpers.rb
@@ -3,7 +3,11 @@ module CornellAssembliesRails
     module MarkdownHelpers
 
       def markdown( content )
-        sanitize Markdown.new(content, tables: true).to_html
+        out = Markdown.new(content, tables: true).to_html
+        { '\+' => 'ins', '\-' => 'del' }.each do |mark, tag|
+          out.gsub! /\[#{mark}(.+)#{mark}\]/, "<#{tag}>$1</#{tag}>"
+        end
+        sanitize out
       end
 
     end
