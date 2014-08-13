e59830b6980cfef23305e633d486fd8eb6a018b2
Ari Epstein <aepstein607@gmail.com>
2014-08-13 18:18:24 -0400

    Move in simple_form configuration for bootstrap v3.

--- lib/cornell_assemblies_rails/rails/engine.rb
+++ lib/cornell_assemblies_rails/rails/engine.rb
@@ -33,6 +33,150 @@ module CornellAssembliesRails
         end
         ::SimpleForm::FormBuilder.send :include, DatetimePickerBuilderMethods
         ::SimpleForm::FormBuilder.send :include, WrappedButton
+        ::SimpleForm.setup do |sf_config|
+          sf_config.error_notification_class = 'alert alert-danger'
+          sf_config.button_class = 'btn btn-default'
+          sf_config.boolean_label_class = nil
+
+          sf_config.wrappers :vertical_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+            b.use :html5
+            b.use :placeholder
+            b.optional :maxlength
+            b.optional :pattern
+            b.optional :min_max
+            b.optional :readonly
+            b.use :label, class: 'control-label'
+
+            b.use :input, class: 'form-control'
+            b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+            b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+          end
+
+          sf_config.wrappers :vertical_file_input, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+            b.use :html5
+            b.use :placeholder
+            b.optional :maxlength
+            b.optional :readonly
+            b.use :label, class: 'control-label'
+
+            b.use :input
+            b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+            b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+          end
+
+          sf_config.wrappers :vertical_boolean, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+            b.use :html5
+            b.optional :readonly
+
+            b.wrapper tag: 'div', class: 'checkbox' do |ba|
+              ba.use :label_input
+            end
+
+            b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+            b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+          end
+
+          sf_config.wrappers :vertical_radio_and_checkboxes, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+            b.use :html5
+            b.optional :readonly
+            b.use :label, class: 'control-label'
+            b.use :input
+            b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+            b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+          end
+
+          sf_config.wrappers :horizontal_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+            b.use :html5
+            b.use :placeholder
+            b.optional :maxlength
+            b.optional :pattern
+            b.optional :min_max
+            b.optional :readonly
+            b.use :label, class: 'col-sm-3 control-label'
+
+            b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
+              ba.use :input, class: 'form-control'
+              ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+              ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+            end
+          end
+
+          sf_config.wrappers :horizontal_file_input, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+            b.use :html5
+            b.use :placeholder
+            b.optional :maxlength
+            b.optional :readonly
+            b.use :label, class: 'col-sm-3 control-label'
+
+            b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
+              ba.use :input
+              ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+              ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+            end
+          end
+
+          sf_config.wrappers :horizontal_boolean, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+            b.use :html5
+            b.optional :readonly
+
+            b.wrapper tag: 'div', class: 'col-sm-offset-3 col-sm-9' do |wr|
+              wr.wrapper tag: 'div', class: 'checkbox' do |ba|
+                ba.use :label_input, class: 'col-sm-9'
+              end
+
+              wr.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+              wr.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+            end
+          end
+
+          sf_config.wrappers :horizontal_radio_and_checkboxes, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+            b.use :html5
+            b.optional :readonly
+
+            b.use :label, class: 'col-sm-3 control-label'
+
+            b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
+              ba.use :input
+              ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+              ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+            end
+          end
+
+          sf_config.wrappers :inline_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+            b.use :html5
+            b.use :placeholder
+            b.optional :maxlength
+            b.optional :pattern
+            b.optional :min_max
+            b.optional :readonly
+            b.use :label, class: 'sr-only'
+
+            b.use :input, class: 'form-control'
+            b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+            b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+          end
+          sf_config.default_wrapper = :horizontal_form
+          sf_config.wrappers :default, class: :input,
+            hint_class: :field_with_hint, error_class: :field_with_errors do |b|
+            b.use :html5
+            b.use :placeholder
+            b.optional :maxlength
+            b.optional :pattern
+            b.optional :min_max
+            b.optional :readonly
+            b.use :label_input
+            b.use :hint,  wrap_with: { tag: :span, class: :hint }
+            b.use :error, wrap_with: { tag: :span, class: :error }
+          end
+          sf_config.default_wrapper = :default
+          sf_config.boolean_style = :nested
+          sf_config.button_class = 'btn'
+          sf_config.error_notification_tag = :div
+          sf_config.error_notification_class = 'error_notification'
+          sf_config.form_class = "simple_form form-horizontal"
+          sf_config.browser_validations = false
+          sf_config.boolean_label_class = 'checkbox'
+        end
       end
     end
 
